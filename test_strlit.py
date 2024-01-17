a58363b9c1b6f136d7bf788ef5d755c85528d586
Nathan Cladiere <nathan.cladiere@gmail.com>
2024-01-17 17:57:45 +0100

    final streamlit

---
+++ test_strlit.py
@@ -0,0 +1,20 @@
+import pytest
+import requests
+from your_streamlit_app import get_image_from_api, get_client_address, predict_for_client
+
+# Constantes pour les tests
+FASTAPI_SERVER = 'https://dsoc-p7-api-019616fdcaac.herokuapp.com'
+TEST_IMAGE_NAME = 'PAYMENT_RATE_kde'
+TEST_CLIENT_ID = 100042  # Remplacez par un ID client valide pour les tests
+
+def test_get_image_from_api():
+    response = get_image_from_api(TEST_IMAGE_NAME)
+    assert response is not None or response is None  # Réussite si l'image est trouvée ou correctement gérée si elle n'existe pas
+
+def test_get_client_address():
+    response = get_client_address(TEST_CLIENT_ID)
+    assert response is not None  # Vérifiez si l'adresse du client est correctement récupérée
+
+def test_predict_for_client():
+    response = predict_for_client(TEST_CLIENT_ID)
+    assert response is not None  # Vérifiez si la prédiction est correctement effectuée
